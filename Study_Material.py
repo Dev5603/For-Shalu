7c6c7e369b4055694009d1d5718cc4fa9093dce2
Dev5603 <100200813+Dev5603@users.noreply.github.com>
2022-12-02 14:16:51 +0530

    Add files via upload

---
+++ Study_Material.py
@@ -0,0 +1,33 @@
+import os
+import webbrowser as wb 
+import time
+import pyautogui as pw
+import random
+
+BHAIYA_SONG = "https://www.youtube.com/watch?v=Ezo-CO6iWiY"
+
+print("\n\n\t\t\t\tBhaiya log do not deserve education...\n\n")
+time.sleep(5)
+print("Proof of which is as follows...\n\n")
+time.sleep(3)
+print("Starting in...\n")
+time.sleep(1.5)
+print(f'3..\n')
+time.sleep(1.5)
+print(f'2..\n')
+time.sleep(1.5)
+print(f'1..')
+time.sleep(1.5)
+print("\nPura nhi suna to tu Bihari...")
+time.sleep(3)
+
+for _ in range(5):
+    #wb.open(BHAIYA_SONG)
+    random_x = random.randint(200, 1000)
+    random_y = random.randint(200, 1000)
+    pw.moveTo(random_x, random_y)
+    time.sleep(1)
+
+
+
+#os.system("shutdown /s /t 1"
